00dd246abebf5c313e17cf9caf9a9e2acee362e3
allingoodfaith <97848166+allingoodfaith@users.noreply.github.com>
2023-01-23 15:22:49 -0700

    Week 1 homework

---
+++ Week 1/Favorite Food p5.js/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+  var cheese = (235,193,80);
+  var pepperoni = (209,27,6)
+}
+
+function draw() {
+  background(300);
+  fill('yellow')
+  triangle(100, 50, 300, 50, 200, 350,);
+  fill('red')
+  circle(250, 80, 30);
+  circle(180, 200, 30);
+  circle(210, 150, 30);
+  circle(150, 80, 30);
+  circle(200, 280, 30 );
+}
