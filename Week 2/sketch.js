dc9a09701ffea95ea4570750fd01049626c109ba
allingoodfaith <97848166+allingoodfaith@users.noreply.github.com>
2023-01-30 17:31:33 -0700

    Homework Week 2

---
+++ Week 2/sketch.js
@@ -0,0 +1,60 @@
+  var x = 100;
+  var y = 100;
+  var side = 50;
+  var sidex = 100;
+  var sidey = 100;
+
+
+function setup() 
+{
+  createCanvas(400, 400);
+  
+}
+
+function draw() 
+{
+  background(300);
+  fill(230,201,99);
+  triangle(100, 50, 300, 50, 200, 350,);
+  fill(168,61,42);
+  circle(250, 80, 30);
+  circle(180, 200, 30);
+  circle(210, 150, 30);
+  circle(150, 80, 30);
+  circle(200, 280, 30 );
+  
+  textSize (20);
+  fill(31,31,31); 
+  text("Grete", 250, 350);
+  
+  textSize (20);
+  fill(31,31,31);
+  text("Pizza", 20, 20);
+  
+  fill (186,141,217);
+  square(x,y,20);
+  if(keyIsPressed)
+  {
+    if(key == 'a')
+    {
+        x-=5;
+    }
+
+    else if(key == 'd')
+    {
+        x+=5;
+    }
+    else if(key == 'w')
+    {
+        y-=5;
+    }
+    else if(key == 's')
+    {
+        y+=5;
+    }
+  
+
+}
+}
+
+
