667303d0a339a3541be765d94900b80f3ae04c46
allingoodfaith <97848166+allingoodfaith@users.noreply.github.com>
2023-02-06 19:31:51 -0700

    edits 02062023 7:30pm

--- Week 3/js/sketch.js
+++ Week 3/js/sketch.js
@@ -8,21 +8,21 @@ var myFont;
 
 //movement variables 
 var moveX = 100;
-var moveY = 100;
+var moveY = 50;
 var xChange = 100;
 
 function preload()
 {
         //loads images and fonts in this function
-    image1 = loadImage("../images/image1.png");
-    image2 = loadImage("../images/image2.png");
-    image3 = loadImage("../images/image3.png");
-    myFont = loadFont("../fonts/Fathia.tff");
+    image1 = loadImage("./images/image1.png");
+    image2 = loadImage("./images/image2.png");
+    image3 = loadImage("./images/image3.png");
+    myFont = loadFont("./fonts/CaviarDreams.tff");
 }
 
 function setup()
 {
-    createCanvas (1500,500);
+    createCanvas (1675,500);
     setInterval(displayCounter,1000);
 }
 
@@ -30,26 +30,27 @@ function draw()
 {
     background (212,185,237);
 
-    image(image1,moveX,50,400,400);
-    image(image2,550,50,300,400);
-    image(image3,1000,50,600,400); 
+    image(image1,moveX,moveY,400,400);
+    image(image2,550,50,275,400);
+    image(image3,875,50,675,400); 
 
     drawCounter();
 
     movement();
 
-    // textFont(myFont);
-    textSize(30);
+    //textFont(myFont);
+    textSize(5);
     fill(0);
     text("some of my artwork in recent years",25, 750);
 }
 
 function movement()
 {
-    if(moveX > height-50 || moveW < 0);
+    if(moveX > height-10 || moveY < 0);
     {
-        xChange *= -1;
+        xChange *= -15;
     }
+    moveX *= moveY;
 }
 
 function drawCounter()
